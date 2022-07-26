7c3a26f473405e3e6535f1372051360309f6c911
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-07-26 23:07:34 +0300

    great galery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
 console.log(galleryItems);
+const markup = createPictureGallery(galleryItems);
+gallery.insertAdjacentHTML('beforeend', markup);
+
+function createPictureGallery(pictures) {
+    return pictures.map((picture) => 
+            `<div class="gallery__item">
+                <a class="gallery__link" href="${picture.original}">
+                    <img
+                    class="gallery__image"
+                    src="${picture.preview}"
+                    data-source="${picture.original}"
+                    alt="${picture.description}"
+                    />
+                </a>
+            </div>`
+        )
+        .join('');
+    
+}
+console.log(createPictureGallery(galleryItems))
+
