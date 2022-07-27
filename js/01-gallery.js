f9194192ec382d849afd5c865bbff63e29092e8a
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-07-27 22:47:49 +0300

    connect  basicLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-const gallery = document.querySelector('.gallery');
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
 const markup = createPictureGallery(galleryItems);
+
+
 gallery.insertAdjacentHTML('beforeend', markup);
+gallery.addEventListener('click', onGalleryPictureClick)
 
 function createPictureGallery(pictures) {
     return pictures.map((picture) => 
@@ -24,3 +26,12 @@ function createPictureGallery(pictures) {
 }
 console.log(createPictureGallery(galleryItems))
 
+function onGalleryPictureClick(evt) {
+    evt.preventDefault()
+
+
+    const instance = basicLightbox.create(`
+	<img src="${evt.target.dataset.source}">
+`)
+    instance.show()
+}
