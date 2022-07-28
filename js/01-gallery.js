d5b3aaca98123771fd9e56c7c70f261c45a9ad7c
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-07-28 18:43:35 +0300

    01 gellery finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-console.log(galleryItems);
+// console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
 const markup = createPictureGallery(galleryItems);
-
+let instance;
 
 gallery.insertAdjacentHTML('beforeend', markup);
 gallery.addEventListener('click', onGalleryPictureClick)
+
 
 function createPictureGallery(pictures) {
     return pictures.map((picture) => 
@@ -24,14 +25,33 @@ function createPictureGallery(pictures) {
         .join('');
     
 }
-console.log(createPictureGallery(galleryItems))
+
 
 function onGalleryPictureClick(evt) {
     evt.preventDefault()
 
-
-    const instance = basicLightbox.create(`
+    instance = basicLightbox.create(`
 	<img src="${evt.target.dataset.source}">
-`)
-    instance.show()
+`, {
+        onShow: (instance) => {
+        window.addEventListener('keydown', onEscKeyPress)
+    },
+        onClose: (instance) => {
+        window.removeEventListener('keydown', onEscKeyPress)
+    }
+    }
+    );
+    instance.show() 
+}
+
+function onEscKeyPress(event) {
+    
+    if (event.code === 'Escape') {
+        onCloseModal();
+    }
+}
+
+function onCloseModal() {
+    instance.close();
+    // document.body.lastElementChild.remove()
 }
